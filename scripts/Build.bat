5dd8487c0e6602a0696e17b9372f7fd3736f9ec5
Aschermer <Github@aschermer.com>
2025-09-16 20:08:01 -0500

    Basic GLFW Window

--- scripts/Build.bat
+++ scripts/Build.bat
@@ -1,6 +1,6 @@
 @echo off
-set "CFLAGS=/nologo /Zi /c /FC /I C:\Home\System\Resources\Include"
-set "LFLAGS=/NOLOGO /DEBUG:FULL /INCREMENTAL:NO /OUT:"debug.exe" /LIBPATH:"C:\Home\System\Resources\Libraries""
+set "CFLAGS=/nologo /Zi /c /FC /EHsc /I C:\Home\System\Resources\Include /I C:\VulkanSDK\1.4.321.1\Include"
+set "LFLAGS=/NOLOGO /DEBUG:FULL /INCREMENTAL:NO /OUT:"debug.exe" /LIBPATH:"C:\Home\System\Resources\Libraries" /LIBPATH:"C:\VulkanSDK\1.4.321.1\Lib""
 
 del /q .\bin\*
 del /q .\build\*
@@ -10,5 +10,5 @@ cl %CFLAGS% ..\code\main.cpp
 popd
 
 pushd .\bin
-link %LFLAGS% ..\build\main.obj user32.lib gdi32.lib kernel32.lib \GLFW\glfw3.lib
+link %LFLAGS% ..\build\main.obj user32.lib gdi32.lib kernel32.lib shell32.lib \GLFW\glfw3_mt.lib vulkan-1.lib
 popd
